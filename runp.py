d9002dd8f8208ee4fbe60e4a8b1e428f90c0d344
Ricardo Ferreira <rmf2pt@gmail.com>
2017-04-28 11:10:52 +0000

    Add unit testing

---
+++ runp.py
@@ -0,0 +1,5 @@
+#!/home/ubuntu/.virtualenvs/venv1/bin/python
+import os
+from app import app
+#app.run(debug=True)
+app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)),debug=False)
